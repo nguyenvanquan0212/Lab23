7c7d944583fcfe81e827cdcc2e60d43e70bffd3d
hoangle2603 <levanhoangck2@gmail.com>
2021-06-25 11:23:37 +0700

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,140 @@
+﻿using Lab2_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2_3.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public string HelloTeacher(string university)
+        {
+            return "Hello Thầy Hùng - University: " + university;
+        }
+
+        public ActionResult ListBook()
+        {
+            var books = new List<string>();
+            books.Add("HTML5 & CSS3 The Complete Manual - Author Name Book 1");
+            books.Add("HTML 5 & CSS3  Responsive web Design CookBook - Author Name Book 2");
+            books.Add("Professional ASP.Net MVC5 - Author Name Book 2");
+            ViewBag.Books = books;
+            return View();
+        }
+
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Book 1", "Nguyễn Du", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "Book 2", "Kim Lân", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "Book 3", "Nguyễn Trãi", "/Content/Images/book3.jpg"));
+            return View(books);
+            return View();
+        }
+
+        public ActionResult EditBook(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Book 1", "Nguyễn Du", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "Book 2", "Kim Lân", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "Book 3", "Nguyễn Trãi", "/Content/Images/book3.jpg"));
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    book = b;
+                    break;
+                }
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("EditBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, string Title, string Author, string Image_cover)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Book 1", "Nguyễn Du", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "Book 2", "Kim Lân", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "Book 3", "Nguyễn Trãi", "/Content/Images/book3.jpg"));
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    b.Title = Title;
+                    b.Author = Author;
+                    b.Image_cover = Image_cover;
+                }
+            }
+            return View("ListBookModel", books);
+        }
+        public ActionResult CreateBook()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("CreateBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact([Bind(Include = "Id, Title, Author, ImageCover")] Book book)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Book 1", "Nguyễn Du", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "Book 2", "Kim Lân", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "Book 3", "Nguyễn Trãi", "/Content/Images/book3.jpg"));
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    books.Add(book);
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Error Save Data");
+            }
+            return View("ListBookModel", books);
+        }
+
+        public ActionResult DeleteBook(int id)
+        {
+            var books = new List<Book>();
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    book = b;
+                    break;
+                }
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("DeleteBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, string Title, string Author, string ImageCover)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Book 1", "Nguyễn Du", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "Book 2", "Kim Lân", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "Book 3", "Nguyễn Trãi", "/Content/Images/book3.jpg"));
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    b.Title = Title;
+                    b.Author = Author;
+                    b.Image_cover = ImageCover;
+                    books.Remove(b);
+                    break;
+                }
+            }
+            return View("ListBookModel", books);
+        }
+
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab2_3.Models
+{
+    public class Book
+    {
+        private int id;
+        private string title;
+        private string author;
+        private string image_cover;
+        public Book()
+        {
+
+        }
+        public Book(int id, string title, string author, string image_cover)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image_cover = image_cover;
+        }
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        [Required(ErrorMessage = "Tiêu đề không được trống")]
+        [StringLength(250, ErrorMessage = "Tiêu đề sách không được quá 250 ký tự")]
+        [Display(Name = "Tiêu đề")]
+        public String Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public String Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public String Image_cover
+        {
+            get { return image_cover; }
+            set { image_cover = value; }
+        }
+
+
+    }
+   
+
+}
